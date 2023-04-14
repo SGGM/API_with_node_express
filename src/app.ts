cd352d93912c674551820e0114605edaca057187
SGGM <gleb_somov@mail.ru>
2023-04-14 16:57:22 +0300

    exeption handler added

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express, { Express } from 'express';
 import { Server } from 'http';
 import { LoggerService } from './logger/logger.service.js';
 import { UserController } from './users/users.controller.js';
+import { ExeptionFilter } from './errors/exeption.filter.js';
 
 export class App {
 	app: Express;
@@ -9,22 +10,31 @@ export class App {
 	port: number;
 	logger: LoggerService;
 	userController: UserController;
+	exeptionFilter: ExeptionFilter;
 
 	constructor(
 		logger: LoggerService,
-		UserController: UserController) {
+		UserController: UserController,
+		exeptionFilter: ExeptionFilter
+	) {
 		this.app = express();
 		this.port = 8000;
 		this.logger = logger;
 		this.userController = UserController;
+		this.exeptionFilter = exeptionFilter;
 	}
 
 	useRoutes() {
 		this.app.use('/users', this.userController.router);
 	}
 
+	useExceptionFilters() {
+		this.app.use(this.exeptionFilter.catch.bind(this.exeptionFilter));
+	}
+
 	public async init() {
 		this.useRoutes();
+		this.useExceptionFilters();
 		this.server = this.app.listen(this.port);
 		this.logger.log(`Server runs on http://localhost:${this.port}`);		
 	}
